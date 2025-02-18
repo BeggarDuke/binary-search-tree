c84f77ace4693e4adfa0865a42a49b351c4f3d18
BeggarDuke <107742417+BeggarDuke@users.noreply.github.com>
2025-02-18 16:30:25 +0300

    init

---
+++ main.js
@@ -0,0 +1,59 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+class Tree {
+  constructor(root) {
+    this.root = root;
+  }
+}
+
+function sortedArrayToBSTRecur(arr, start, end) {
+  if (start > end) return null;
+
+  // Find the middle element
+  let mid = start + Math.floor((end - start) / 2);
+
+  // Create root node
+  let root = new Node(arr[mid]);
+  
+  // Create left subtree
+  root.left = sortedArrayToBSTRecur(arr, start, mid - 1);
+
+  // Create right subtree
+  root.right = sortedArrayToBSTRecur(arr, mid + 1, end);
+
+  return root;
+}
+
+function sortAndToBST(arr) {
+  const newArr = arr.sort((a, b) => a - b);
+  const finalArr = [];
+  for (let i = 0; i < newArr.length; i++) {
+    if (newArr[i] !== newArr[i+1]) {
+      finalArr.push(newArr[i]);
+    }
+  }
+  return sortedArrayToBSTRecur(finalArr, 0, finalArr.length - 1);
+}
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+
+let test = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+let testRoot = sortAndToBST(test);
+prettyPrint(testRoot);

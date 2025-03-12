3b3f2fb38f19a70c1cc36fa5bed621fc4e16641e
BeggarDuke <107742417+BeggarDuke@users.noreply.github.com>
2025-03-12 03:32:25 +0300

    doc: add comments to tree methods

--- tree.js
+++ tree.js
@@ -8,21 +8,35 @@ class Node {
   }
 }
 class Tree {
-  constructor(arr) {
-    this.root = Tree.sortAndFillTree(arr, this);
+
+  // is sorted is not true - use static method sortAndFillTree to sort arr before send it to 
+  // another static method sortedArrayToBSTRecur.
+  // if sorted is true - immediately use static method sortedArrayToBSTRecur.
+  // if arguments wasn't provided at all - sortAndFillTree will assign new empty Node to the root
+  constructor(arr, sorted) {
+    this.root = (!sorted) ? Tree.sortAndFillTree(arr) : Tree.sortedArrayToBSTRecur(arr);
   }
-  static sortAndFillTree(arr, tree) {
+
+  // simple sort and remove duplicates. 
+  static sortAndFillTree(arr) {
     if (!arr) return new Node(null);
     const newArr = arr.sort((a, b) => a - b);
     const finalArr = [];
+
+    // Removing duplicates implemented by checking if item at the next index in the sorted arr (newArr)
+    // is equal to item at the current index, if it is - just ignore that item, otherwise - push item to
+    // the finalArr. (in a sorted array every equal items are placed one after the other. So if next item is not
+    // equal to the current item, then current item is the last or only item with that value and we can push it in
+    // the final array) 
     for (let i = 0; i < newArr.length; i++) {
       if (newArr[i] !== newArr[i+1]) {
         finalArr.push(newArr[i]);
       }
     }
-    return tree.sortedArrayToBSTRecur(finalArr, 0, finalArr.length - 1);
+    return Tree.sortedArrayToBSTRecur(finalArr);
   }
-  sortedArrayToBSTRecur(arr, start, end) {
+  // simple BST recursive function
+  static sortedArrayToBSTRecur(arr, start = 0, end = arr.length - 1) {
     if (start > end) return null;
   
     // Find the middle element
@@ -32,21 +46,26 @@ class Tree {
     let root = new Node(arr[mid]);
     
     // Create left subtree
-    root.left = this.sortedArrayToBSTRecur(arr, start, mid - 1);
+    root.left = Tree.sortedArrayToBSTRecur(arr, start, mid - 1);
   
     // Create right subtree
-    root.right = this.sortedArrayToBSTRecur(arr, mid + 1, end);
+    root.right = Tree.sortedArrayToBSTRecur(arr, mid + 1, end);
   
     return root;
   }
+  // insert new Node to the tree with given data
   insert(data) {
     let node = this.root;
+    // if root node doesn't have any data - update it's data with given data
     if (!node.data) {
       this.root.data = data;
       return;
     }
     while (node !== null) {
+      // check for duplicates
       if (data === node.data) return;
+      // compare given data with node.data and traverse according to tree rules
+      // if the next subtree is equal to null - assign new Node with given data on the place of null
       if (data < node.data) {
         if (node.left === null) {
           node.left = new Node(data);
@@ -62,15 +81,18 @@ class Tree {
       }
     }
   }
+  // delete Node with given data
   delete(data) {
     let current = this.search(data);
     if (current === null) return;
+    // if node with given data doesn't have any children - delete that node from parents refs.
     if (current.nodeChildren === 0) {
       (current.parent.left !== null && current.parent.left.data === data) 
       ? current.parent.left = null 
       : current.parent.right = null;
       return;
     }
+    // if node with given data has only one child - simply put that child node at the place of node that should be deleted
     if (current.nodeChildren === 1) {
       let newNode = (current.node.left !== null) 
       ? current.node.left 
@@ -79,6 +101,9 @@ class Tree {
       ? current.parent.left = newNode
       : current.parent.right = newNode;
     } else {
+    // if node with given data has two children - find a suitable candidate to take it's place
+    // by searching for a lowest node in the right subtree of node that should be deleted
+    // (current node > go to the right subtree > in that right subtree go to the left subtree until find the end)
       let parent = null;
       let newNode = current.node.right;
       while (newNode.left !== null) {
@@ -87,16 +112,22 @@ class Tree {
       }
       if (parent !== null) {
         current.node.data = newNode.data;
-        parent.left = newNode.right;
+        // if the lowest node has right subtree as its child - it will be appended to its parent left subtree
+        // because values of that subtree is still lower than data of that parent
       } else {
+        // if the lowest node is the first node in the right subtree of the current node then
+        // assign data from lowest node to the current node, and assign right subtree of the lowest node
+        // as right subtree of the current node.
         current.node.data = newNode.data;
         current.node.right = newNode.right;
       }
     }
   }
+  // search the given data and return object with information about node with that data, its parent, amount of children and depth(line 211)
   search(data) {
     let node = this.root;
     let depth = 1;
+    // check amount of children of given node
     const childrenCheck = (node) => {
       let nodeChildren = 0;
       if (node.left !== null) nodeChildren++;
@@ -111,7 +142,9 @@ class Tree {
         depth: depth,
       }
     }
+    // traverse until find given data
     while (node !== null) {
+      // increase depth with each iteration
       depth++;
       if (node.left !== null && data === node.left.data) {
         return {
@@ -129,10 +162,13 @@ class Tree {
           depth: depth,
         }
       }
+      // traverse according to comparison between data and node.data
       (data < node.data) ? node = node.left : node = node.right;
     }
     return null;
   }
+  // traverse in levelOrder and call callback on each node.
+  // levelOrder traversal is implemented with simple queue(first-in-first-out).
   levelOrder(callback) {
     if (!callback || typeof callback !== "function") throw new Error("no valid callback is provided");
     let q = [this.root];
@@ -145,31 +181,35 @@ class Tree {
       current++;
     }
   }
+  // simple recursive preOrder traversal and call a callback to each node
   preOrder(callback, node = this.root) {
     if (!callback || typeof callback !== "function") throw new Error("no valid callback is provided");
     callback(node);
     if (node.left) this.preOrder(callback, node.left);
     if (node.right) this.preOrder(callback, node.right);
   }
+  // as above, but inOrder traversal
   inOrder(callback, node = this.root) {
     if (!callback || typeof callback !== "function") throw new Error("no valid callback is provided");
     if (node.left) this.inOrder(callback, node.left);
     callback(node);
     if (node.right) this.inOrder(callback, node.right);
   }
+  // as above, but postOrder traversal
   postOrder(callback, node = this.root) {
     if (!callback || typeof callback !== "function") throw new Error("no valid callback is provided");
     if (node.left) this.postOrder(callback, node.left);
     if (node.right) this.postOrder(callback, node.right);
     callback(node);
   }
+  // recursive method that is checking how many steps until the end of deepest tree from the given node.
   height(node) {
     let data = this.search(node).node;
     const findHeight = function(node) {
       let left = 0;
       let right = 0;
-      if (node.left)left += findHeight(node.left) + 1;
-      if (node.right)right += findHeight(node.right) + 1;
+      if (node.left) left += findHeight(node.left) + 1;
+      if (node.right) right += findHeight(node.right) + 1;
       if (!node.left && !node.right) {
         return 1;
       }
@@ -178,9 +218,11 @@ class Tree {
     }
     return findHeight(data);
   }
+  // use search method on given node to get the info of how deep is given node nested in the tree 
   depth(node) {
     return this.search(node).depth;
   }
+  // check if a tree is balanced (balanced tree is a tree that have difference in subtrees depth no more than 1)
   isBalanced() {
     const findHeight = function(node) {
       let obj = {
@@ -214,10 +256,13 @@ class Tree {
         return false;
     } else return true;
   }
+  // rebalance a tree by recreating a tree completely.
+  // inOrder method traverse from lowest to highest value
+  // so arr will be immediately sorted
   rebalance() {
     let arr = []; 
     this.inOrder((node) => arr.push(node.data));
-    this.root = this.sortedArrayToBSTRecur(arr, 0, arr.length - 1)
+    this.root = Tree.sortedArrayToBSTRecur(arr, 0, arr.length - 1)
     return arr;
   }
 }
